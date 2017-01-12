7aeaf582f0a22a2955c34ee11d6bd03b0cf6804e
John Campbell <johnmarcampbell@gmail.com>
2017-01-12 18:01:27 +0000

    Modify throttling and logging settings

--- congress_gov/runSpider.py
+++ congress_gov/runSpider.py
@@ -3,15 +3,18 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from congress_gov.spiders.congress import CongressSpider as cs
 import arrow
+import logging
 
-process = CrawlerProcess(get_project_settings())
+settings = get_project_settings()
+settings['LOG_FILE'] = 'log.txt'
+
+process = CrawlerProcess(settings)
 
 date_format = 'MM/DD/YYYY'
 start_date = arrow.get('01/01/2016',date_format)
 end_date = arrow.get('01/31/2016', date_format)
 
 for day in arrow.Arrow.range('day', start_date, end_date):
-    print(day.format(date_format))
 
     process.crawl(cs, date=day.format(date_format), item_limit=999)
 
