c46680099378cf0fe1704198c5c6fab4307258ff
John Campbell <johnmarcampbell@gmail.com>
2017-03-18 12:31:52 -0400

    Add some helper functions for parsing record text

--- concord/recordparser.py
+++ concord/recordparser.py
@@ -13,3 +13,23 @@ class RecordParser(object):
         clean_text = re.sub(page_break, '\n', self.text)
         self.text = clean_text
 
+    # Remove line breaks that cut paragraphs in half
+    def remove_midline_returns(self):
+        mid_line_returns = '\n(\w)'
+        clean_text = re.sub(mid_line_returns, '\\1', self.text)
+        self.text = clean_text
+
+    # Split document around blank lines
+    # to get multi-paragraph sections
+    def get_sections(self):
+        sections = []
+        paragraph = ''
+        for line in self.text.splitlines():
+            paragraph += line
+            if line == '':
+                sections.append(paragraph)
+                paragraph = ''
+
+        # Throw away empty sections
+        sections = [val for val in sections if val != '']
+        return sections
