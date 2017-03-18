1c1d548f690bebd0b2fd72798878c02f71ab0a48
John Campbell <johnmarcampbell@gmail.com>
2017-03-18 05:06:52 -0400

    Add get_payload() function to BioguideSearch

--- bioguide/bioguide_search.py
+++ bioguide/bioguide_search.py
@@ -1,3 +1,5 @@
+import requests
+
 class BioguideSearch(object):
     """An object that will perform searches on the Biographical
     Directory of the US Congress"""
@@ -6,6 +8,22 @@ class BioguideSearch(object):
         """Set some defaults"""
         self.settings = self.get_settings(**kwargs)
 
+    def get_payload(self):
+        """This function goes through the self.settings dictionary and
+        picks out the settings that correspond to a payload of search
+        parameters"""
+
+        payload = dict(
+            first_name=self.settings['first_name'],
+            last_name=self.settings['last_name'],
+            position=self.settings['position'],
+            state=self.settings['state'],
+            party=self.settings['party'],
+            year_or_congress=self.settings['year_or_congress']
+            )
+
+        return payload
+        
     def get_settings(self, **kwargs):
         """Turn a set of keyword arguments into a dictionary of settings """
         # Dictionary with default values
