e684f19fa079558d5b15fbdc29360f2ca33ac1ba
John Campbell <johnmarcampbell@gmail.com>
2017-03-18 10:35:25 -0400

    Add template member and appointment objects

---
+++ concord/appointment.py
@@ -0,0 +1,7 @@
+class Appointment(object):
+    """This object represents one appointment to congress """
+
+    def __init__(self, position='', party='', state='', congress='', 
+        start_year='', end_year=''):
+        """Set some values"""
+    
---
+++ concord/member.py
@@ -0,0 +1,7 @@
+class Member(object):
+    """This object represents one member of congress"""
+
+    def __init__(self, last_name='', first_name='', middle_name='',
+        bioguide_id='', appointments=[]):
+        """Set some values"""
+    
