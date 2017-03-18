27d899bcb8cc32e6a56056172c1476ff2f1748fb
John Campbell <johnmarcampbell@gmail.com>
2017-03-18 12:44:39 -0400

    Add some helper functions for identifying a speaker

--- concord/recordparser.py
+++ concord/recordparser.py
@@ -33,3 +33,56 @@ class RecordParser(object):
         # Throw away empty sections
         sections = [val for val in sections if val != '']
         return sections
+
+    # Look at the beginning of a section to see if
+    # it corresponds to someone speaking. If it does,
+    # figure out who
+    def get_speaker(self, section):
+        titles = '(?:The|Mr\.|Mrs\.|Ms\.)'
+        caps_name_or_title = '([A-Z][A-Za-z ]+)'
+        paren_name = '(?:\(([A-Za-z. ]+)\))?'
+        speaker_string = '  {} {}{}\.'.format(titles, caps_name_or_title, paren_name)
+
+        speaker_match = re.match(speaker_string, section)
+        if self.match_is_good(speaker_match):
+            if speaker_match.group(2):
+                speaker = speaker_match.group(2).lower()
+            else: 
+                speaker = speaker_match.group(1).lower()
+        else:
+            speaker = None
+
+        # Check for 'Mr. Foo of Bar' and get 'Foo'
+        if speaker:
+            of_match = re.search('([a-z]+) of ', speaker)
+            if of_match:
+                speaker = of_match.group(1)
+
+        # Check for 'Mr. Foo' and get 'Foo'
+        if speaker:
+            title_match = re.match('(?:mr\.|mrs\.|ms\.) (\w+)', speaker)
+            if title_match:
+                speaker = title_match.group(1)
+
+        return speaker
+
+    # After getting a speaker match from
+    # get_speaker() (below), double-check
+    # a few things to remove false positives
+    def match_is_good(self, match):
+        ignore_phrases = ['chair recognizes', 'during the vote', 'committee resumed']
+        word_limit = 7
+        match_is_good = True
+
+        if match:
+            for text in match.groups():
+                if text and (len(text.split()) > word_limit):
+                    match_is_good = False
+            for phrase in ignore_phrases:
+                if text and re.match(phrase, text.lower()):
+                    match_is_good = False
+        else:
+            match_is_good = False
+
+
+        return match_is_good
